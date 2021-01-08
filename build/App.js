a478b7d57518c0d4a963d34ac2b88dd323f257ca
Christophe Hurpeau <christophe@hurpeau.com>
2021-01-08 14:29:55 +0100

    chore: remove build directory

--- build/App.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var MyView_1 = require("./MyView");
-var Person_1 = require("./Person");
-function App() {
-    return new MyView_1.MyView(new Person_1.Person("Melissa", "Buhr")).render();
-}
-exports["default"] = App;
